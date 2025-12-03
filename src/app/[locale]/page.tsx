93ca4b03f224f1fc1c6c8ae2112305b0617fbcdc
renan.prado <renan.prado@mesainc.com.br>
2025-12-03 15:06:00 -0300

    feat: update homepage to display 'coming soon' message and localize text in English and Portuguese

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,17 @@
+import { Icon } from '@core/ui/icons'
+import { BRAND } from '@reglo-co/shared/consts'
 import { useTranslations } from 'next-intl'
 
 export default function Home() {
   const t = useTranslations('app')
 
   return (
-    <main>
-      <h1>{t('hello')}</h1>
+    <main className="flex flex-col items-center justify-center h-screen gap-10 pt-6 bg-[#D9D3F1]">
+      <div className="flex items-center gap-6 self-center">
+        <Icon.BrandLogoSquare className="size-14" />
+        <span className="font-bold font-mono text-4xl">{BRAND.NAME.toUpperCase()}</span>
+      </div>
+      <h1 className="text-xl font-thin opacity-50">{t('coming-soon')}...</h1>
     </main>
   )
 }
