f073c8535998e95c157766888d1fa45444df96d7
renan.prado <renan.prado@mesainc.com.br>
2025-12-03 15:12:59 -0300

    deploy

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,17 +1,16 @@
 import { Icon } from '@core/ui/icons'
-import { BRAND } from '@reglo-co/shared/consts'
 import { useTranslations } from 'next-intl'
 
 export default function Home() {
   const t = useTranslations('app')
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen gap-10 pt-6 bg-[#D9D3F1]">
+    <main className="flex h-screen flex-col items-center justify-center gap-10 bg-[#D9D3F1] pt-6">
       <div className="flex items-center gap-6 self-center">
         <Icon.BrandLogoSquare className="size-14" />
-        <span className="font-bold font-mono text-4xl">{BRAND.NAME.toUpperCase()}</span>
+        <span className="font-bold font-mono text-4xl">REGLO</span>
       </div>
-      <h1 className="text-xl font-thin opacity-50">{t('coming-soon')}...</h1>
+      <h1 className="font-thin text-xl opacity-50">{t('coming-soon')}...</h1>
     </main>
   )
 }
